0133a0081cc89f2cf0c58313bd8cd5330635c244
Daniil Borysov <daniil.borysov@techhow.studio>
2021-04-17 12:45:14 +0300

    Section 5 finished

--- modules/Bootcamp.js
+++ modules/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -11,7 +13,7 @@ const BootcampSchema = new mongoose.Schema({
     slug: String,
     description: {
         type: String,
-        required: [true, 'Please add a desctiption'],
+        required: [true, 'Please add a description'],
         maxlength: [500, 'Desctiption cannot be more than 50 characters'],
     },
     website: {
@@ -97,6 +99,31 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function (next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode,
+    };
+
+    // Don't save address in DB
+    this.address = undefined;
+    next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
