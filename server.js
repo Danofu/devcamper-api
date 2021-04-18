3b0218c4653762206f5a47a144d9f683b527073e
Daniil Borysov <daniil.borysov@techhow.studio>
2021-04-18 18:28:01 +0300

    Section 6.1 finished

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 // Routes files
-const bootcamp = require('./routes/bootcamps');
-const { connect } = require('mongoose');
+const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
 
 const app = express();
 
@@ -26,7 +26,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamp);
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
 
