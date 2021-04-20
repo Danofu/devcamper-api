15d7ec7c02d17deb3698594fa24b0d57ac595ab4
Daniil Borysov <daniil.borysov@techhow.studio>
2021-04-20 18:09:47 +0300

    Section 8 finished

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth');
+const {
+    register,
+    login,
+    getMe,
+    forgotPassword,
+    resetPassword,
+    updateDetails,
+    updatePassword,
+} = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -8,6 +16,10 @@ const { protect } = require('../middleware/auth');
 router
     .post('/register', register)
     .post('/login', login)
-    .get('/me', protect, getMe);
+    .get('/me', protect, getMe)
+    .post('/forgotpassword', forgotPassword)
+    .put('/resetpassword/:resettoken', resetPassword)
+    .put('/updatedetails', protect, updateDetails)
+    .put('/updatepassword', protect, updatePassword);
 
 module.exports = router;
